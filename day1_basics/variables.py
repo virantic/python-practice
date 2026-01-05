bf74805f192ef5bcd5388b64ea3d6d53044fcf0f
virantic <victoriatheophilus05@gmail.com>
2026-01-05 13:44:17 +0100

    Day 1: variables and data types

--- day1_basics/variables.py
+++ day1_basics/variables.py
@@ -3,8 +3,12 @@ age = 20
 country = "Nigeria"
 height = 1.75
 is_student = True
+favouriteFood = "Yam and egg"
+favouriteColor = "Blue"
 print(name)
 print(age)
 print(country)
 print(height)
 print(is_student)
+print("My favourite food is", favouriteFood)
+print("My favourite color is", favouriteColor)
